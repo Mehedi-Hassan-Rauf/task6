6465f41d184ec3d65901d62e229cd333a18ee585
Rauf <raufnx@gmail.com>
2024-09-30 13:04:40 +0600

    neccessary

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,3 @@ io.on('connection', socket => {
         })
     })
 });
-
-
-// backend/api/index.js
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from the backend!' });
-};
