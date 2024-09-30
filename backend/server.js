ecef4bdb2ebbf240351530b73d8bdff387a910b9
Rauf <raufnx@gmail.com>
2024-09-30 12:00:59 +0600

    path added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import { Server } from 'socket.io';
+import path from "path";
+import dotenv from "dotenv";
 
 import Connection from './database/db.js';
 
 import { getDocument, updateDocument } from './controller/document-controller.js'
 
 const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve();
+dotenv.config();
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 Connection();
 
@@ -12,6 +23,7 @@ const io = new Server(PORT, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
+        credentials:true,
     }
 });
 
